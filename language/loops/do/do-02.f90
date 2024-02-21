ee4cd513975e9e109402ab9fc78062b9a59c4fbd
Paul Ramnora <pramnora@yahoo.com>
2024-02-21 20:09:07 +0000

    Create do-02.f90
    This 'do/end do' code block counts from 1 up to 10...; just like using other programming languages for code block.

---
+++ language/loops/do/do-02.f90
@@ -0,0 +1,13 @@
+program fortran_loop
+
+  ! ...This loop counts upwards going from: 1 to 10'; then, immediately, stops.  
+  !  I got the code by going to: bing.com/AI based search engine;  
+  !  then, asking it to give me Fortran code that counts from 1 to 10.
+  
+  implicit none
+  integer :: i
+  do i = 1, 10
+  print *, i
+  end do
+
+end program fortran_loop
