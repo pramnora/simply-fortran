0d55ee181c1f7899f234bb069b550851b609a7ab
Paul Ramnora <pramnora@yahoo.com>
2024-02-21 19:05:45 +0000

    Create add01.f90
    Initial commit: [add01.f9] Program shows how to declare 3 integer variables on one same line/assign values all 3 of these variables/then, finally, print out what is their sum total result.

---
+++ language/add/add01.f90
@@ -0,0 +1,19 @@
+program add
+    
+    implicit none
+
+    ! declare variables...
+
+    integer::a,b,c
+
+    ! initialise variables...
+    
+    a=1
+    b=2
+    c=a+b
+
+    ! print out variable value...
+    
+    print *, c  !...sum total variable c prints out the number: 3
+
+end program add
