e5b79125ea8250fd282789d03effbf507ce7fe04
Paul Ramnora <pramnora@yahoo.com>
2024-02-22 01:32:10 +0000

    Update prob01.f90
    Delete an unnecessary empty line...; also, added an alternative code comment fix: n**2

--- language/maths/problems/prob01.f90
+++ language/maths/problems/prob01.f90
@@ -5,10 +5,9 @@ program test03
 ! Learn Fortran Problem Solving - 5 | Fortran Tutorial 19
 ! https://www.youtube.com/watch?v=nHgZy91XtPg
 
-
 ! Q: print out... 
 ! 1> Print numbers: 1,2,3,4,5...(1 up to 5: n+1)
-! 2> Print numbers: 1,4,9,16,25 (sum of squares: 1 up to 5: n*n)
+! 2> Print numbers: 1,4,9,16,25 (sum of squares: 1 up to 5: n*n/or, n**2)
 ! 3> Print numbers: 1,8,27,64,125 (numbers cubed: n*n*n/or, n**3)
 
 ! I managed to solve each problem all on my own...; 
